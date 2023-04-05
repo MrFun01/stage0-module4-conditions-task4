88f66701a4325e4a09a73939c5e004654afe1c24
MrFun01 <hprobook48@gmail.com>
2023-04-05 23:57:30 +0500

    Finished task 4 on the module 4

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int res=first>second?first:second;
+        int res2=res>third?res:third;
+        System.out.println(res2);
     }
 }
